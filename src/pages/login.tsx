477c6d0fba85e8616bef9b985a723c1b7f4e7f6e
HP <pouryazeidi@yahoo.com>
2024-09-26 16:30:50 +0300

    change bg size and opacity of cnetral div

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -5,10 +5,10 @@ import Link from 'next/link';
 const Login = () => {
   return (
     <div className="relative min-h-screen flex items-center justify-center">
-      <div className="absolute inset-0 bg-cover bg-center min-h-screen bg-[url('/MainLogomobile.jpg')] sm:bg-[url('/MainLogo.jpg')]">
+      <div className="absolute inset-0  bg-center min-h-screen bg-[url('/MainLogomobile.jpg')] sm:bg-[url('/MainLogo.jpg')] bg-[length:160%]">
       </div>
       <div className="absolute inset-0 bg-black opacity-75"></div>
-      <div className="relative text-black w-full max-w-[350px] md:py-5 mx-3 my-5 bg-black sm:px-5 shadow-smallShadow bg-opacity-10 backdrop-filter backdrop-blur-[8px] rounded-md flex flex-col p-3 space-y-4">
+      <div className="relative text-black w-full max-w-[350px] md:py-5 mx-3 my-5 bg-black sm:px-5 shadow-smallShadow bg-opacity-10 backdrop-filter backdrop-blur-[12px] rounded-md flex flex-col p-3 space-y-4">
         <div className="flex justify-between items-center w-[100%]">
           <h2 className="text-center text-[25px] font-bold text-white">Login</h2>
           <img src="/YadeamLogo.svg" alt="Yadeam Logo"
@@ -30,12 +30,12 @@ const Login = () => {
           </Link>
         </div>
         <div className='flex justify-center'>
-        <button className=" bg-primary2 duration-500 text-white px-2 py-1 hover:scale-[102%] text-[16px] rounded-[40px] mt-10 hover:bg-primary1 hover:transition-all w-full max-w-[400px] ">
-          Login
-        </button>
+          <button className="bg-primary2 duration-500 text-white px-2 py-1 hover:scale-[102%] text-[16px] rounded-[40px] mt-10 hover:bg-primary1 hover:transition-all w-full max-w-[400px] ">
+            Login
+          </button>
         </div>
         <div className="mt-auto flex justify-center">
-          <button className="flex items-center justify-center w-full max-w-[400px] duration-500  bg-white text-black px-2 py-1 rounded-[40px] hover:scale-[102%] hover:transition-all  mt-2 sm:mt-5 md:mt-2 text-[12px] font-semibold sm:text-[16px] sm:font-normal">
+          <button className="flex items-center justify-center w-full max-w-[400px] duration-500 bg-white text-black px-2 py-1 rounded-[40px] hover:scale-[102%] hover:transition-all mt-2 sm:mt-5 md:mt-2 text-[12px] font-semibold sm:text-[16px] sm:font-normal">
             <img src="/gmail.png" alt="Google Icon" className="mr-2 w-5 h-5" />
             Login with Gmail
           </button>
@@ -43,14 +43,14 @@ const Login = () => {
         <div className="mt-3 text-center text-white">
           <span className='text-[13px]'>
             Don't have an account?{' '}
-            <Link href="/register" className="text-primary2  hover:underline font-bold">
+            <Link href="/register" className="text-primary2 hover:underline font-bold">
               Create a new account
             </Link>
           </span>
         </div>
-          <Link href="/" className= "text-primary3 duration-500 border border-transparent text-center hover:border hover:border-primary3 rounded-[40px] p-1">
-            Back
-          </Link>
+        <Link href="/" className="text-primary3 duration-500 border border-transparent text-center hover:border hover:border-primary3 rounded-[40px] p-1">
+          Back
+        </Link>
       </div>
     </div>
   );
