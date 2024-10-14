8a8ee48bf9af9e8f191f997b02b05627f078fc82
HP <pouryazeidi@yahoo.com>
2024-10-14 23:33:55 +0300

    make raise hero more responsive and some changes

--- src/components/RaiseHero.tsx
+++ src/components/RaiseHero.tsx
@@ -4,21 +4,21 @@ import CustomButton from '@/components/Buttons/CustomButton';
 const RaiseHero = () => {
   return (
     <div className="flex flex-col md:flex md:flex-row w-full justify-center md:h-[75vh] h-auto bg-primary2 p-2 md:p-0 mb-2">
-      <div className=" mt-5 sm:mt-2 md:mt-0 flex flex-row md:flex-col items-start justify-center w-full sm:w-full md:w-[38%] ">
+      <div className=" mt-5 sm:mt-2 md:mt-0 flex flex-col  md:flex-col items-start justify-center w-[80%] m-auto  md:m-0 sm:w-[80%] md:w-[38%] ">
         <h1 className="lg:text-[67px] md:text-[50px] sm:text-[36px] text-[23px] font-bold w-[70%] md:w-[80%]  text-white mb-4">Why Raise with Yadeam?</h1>
        
         <CustomButton 
           text="Apply to Raise" 
           onClick={() => alert('Navigating to Raise Application')}
           theme="primary"
-          className=" !w-32 md:!w-36 text-[15px] p-2   sm:text-lg !bg-white !text-primary1 rounded-md text-nowrap"
+          className=" !w-32 md:!w-36 text-[15px] p-2 mb-4 md:mb-0  sm:text-lg !bg-white !text-primary1 rounded-md text-nowrap"
         />
       </div>
-      <div className="w-full md:w-[40%] m-auto md:m-0 flex items-end relative">
+      <div className="w-full md:w-[40%] m-auto md:m-0 relative">
         <img 
           src="/YadeamHero.png" 
           alt="Yadeam image" 
-          className="rounded-lg object-cover shadow-lg w-[95%] md:w-[70%] m-auto md:m-0  md:absolute md:bottom-[-20px] "
+          className="rounded-lg object-cover shadow-lg w-[80%] md:w-[85%] m-auto md:m-0  md:absolute md:bottom-[-20px] "
         />
       </div>
     </div>
