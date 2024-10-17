60dfc1e75b0e05b1d05a8ae986648920475db9a0
HP <pouryazeidi@yahoo.com>
2024-10-17 18:19:29 +0300

    change texts on textarea

--- src/components/TextSection.tsx
+++ src/components/TextSection.tsx
@@ -7,20 +7,20 @@ const TextSection = () => {
        At Yadeam, we provide more than just a platform for raising funds-we offer tailored solutions designed to fuel your startup's growth. Here's why founders trust us to bring their vision to life:        </p>
        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
         <div className="bg-white p-6 shadow-md rounded-lg">
-          <h3 className="text-2xl font-bold text-CTA mb-2">Expert Support</h3>
-          <p className="text-gray-700">From the initial setup to the final stages of your raise, our team is here to guide you through the process.</p>
+          <h3 className="text-[20px] sm:text-[27px] font-bold text-CTA mb-2">Expert Support</h3>
+          <p className="text-gray-700 text-[14px] sm:text-[22px]">From the initial setup to the final stages of your raise, our team is here to guide you through the process.</p>
         </div>
         <div className="bg-white p-6 shadow-md rounded-lg">
-          <h3 className="text-2xl font-bold text-CTA mb-2">Global Exposure</h3>
-          <p className="text-gray-700">Gain access to a vast network of local and international investors to support your business.</p>
+          <h3 className="text-[20px] sm:text-[27px] font-bold text-CTA mb-2">Global Exposure</h3>
+          <p className="text-gray-700 text-[14px] sm:text-[22px]">Gain access to a vast network of local and international investors to support your business.</p>
         </div>
         <div className="bg-white p-6 shadow-md rounded-lg">
-          <h3 className="text-2xl font-bold text-CTA mb-2">Ethical and Transparent</h3>
-          <p className="text-gray-700">We prioritize transparency and ethical practices to align with your company’s mission.</p>
+          <h3 className="text-[20px] sm:text-[27px] font-bold text-CTA mb-2">Ethical and Transparent</h3>
+          <p className="text-gray-700 text-[14px] sm:text-[22px]">We prioritize transparency and ethical practices to align with your company’s mission.</p>
         </div>
         <div className="bg-white p-6 shadow-md rounded-lg">
-          <h3 className="text-2xl font-bold text-CTA mb-2">Data-Driven Success</h3>
-          <p className="text-gray-700">We use advanced analytics to help optimize your campaign and reach your goals faster.</p>
+          <h3 className="text-[20px] sm:text-[27px] font-bold text-CTA mb-2">Data-Driven Success</h3>
+          <p className="text-gray-700 text-[14px] sm:text-[22px]">We use advanced analytics to help optimize your campaign and reach your goals faster.</p>
         </div>
       </div>
     </div>
