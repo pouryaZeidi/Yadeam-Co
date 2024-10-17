531d01ea85749e71d71c11cc2b920ae2949cae2c
HP <pouryazeidi@yahoo.com>
2024-10-17 18:14:27 +0300

    some text size changes

--- src/components/FundingOption.tsx
+++ src/components/FundingOption.tsx
@@ -8,8 +8,8 @@ interface FundingOptionProps {
 const FundingOption: React.FC<FundingOptionProps> = ({ title, description }) => {
   return (
     <div className="bg-primary1 p-6 rounded-lg shadow-lg hover:shadow-xl transition-shadow">
-      <h3 className="text-xl sm:text-2xl font-semibold  sm:font-bold text-white mb-3">{title}</h3>
-      <p className='text-white'>{description}</p>
+      <h3 className="text-[21px] sm:text-[28px] font-semibold  sm:font-bold text-white mb-3">{title}</h3>
+      <p className='text-white text-[14px] sm:text-[23px]'>{description}</p>
     </div>
   );
 };
